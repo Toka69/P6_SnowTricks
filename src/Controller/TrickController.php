2f02d52eba2b9772dc0d15a2caf5c20f1e85bbf5
Matthias LEROUX <matthiasleroux@laposte.net>
2021-01-20 08:31:11 +0100

    Add a category method with its template to show a dedicated page for each category with its tricks. Also adding the content of the desired trick to the template.

--- src/Controller/TrickController.php
+++ src/Controller/TrickController.php
@@ -2,21 +2,55 @@
 
 namespace App\Controller;
 
+use App\Repository\CategoryRepository;
+use App\Repository\TrickRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 
 class TrickController extends AbstractController
 {
     /**
-     * @Route("/group/trick/{slug}",name="trick_show")
+     * @Route("/{slug}", name="trick_category")
      * @param $slug
+     * @param CategoryRepository $categoryRepository
      * @return Response
      */
-    public function show($slug): Response
+    public function category($slug, CategoryRepository $categoryRepository): Response
     {
-        return $this->render('trick/show.html.twig', [
+        $category = $categoryRepository->findOneBy([
             'slug' => $slug
+        ]);
+
+        if(!$category){
+            throw $this->createNotFoundException("Category unavailable");
+        }
+
+        return $this->render('trick/category.html.twig', [
+            'slug' => $slug,
+            'category' => $category
+        ]);
+    }
+
+    /**
+     * @Route("/{category_slug}/{slug}",name="trick_show")
+     * @param $slug
+     * @param TrickRepository $trickRepository
+     * @return Response
+     */
+    public function show($slug, TrickRepository $trickRepository): Response
+    {
+        $trick = $trickRepository->findOneBy([
+            'slug' => $slug
+        ]);
+
+        if(!$trick){
+            throw $this->createNotFoundException("Trick unavailable");
+        }
+
+        return $this->render('trick/show.html.twig', [
+            'trick' => $trick
         ]);
     }
 }
