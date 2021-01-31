31863a2a98046cb252eea2734577d015febbe149
Matthias LEROUX <matthiasleroux@laposte.net>
2021-01-31 20:25:06 +0100

    setting up ajax "load more"

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,12 +2,17 @@
 
 namespace App\Controller;
 
-use App\Entity\Photo;
 use App\Repository\TrickRepository;
 use ArrayObject;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Encoder\XmlEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
 
 class HomeController extends AbstractController
 {
@@ -26,9 +31,33 @@ class HomeController extends AbstractController
             $arrayobj->append($cover);
         }
 
-
         return $this->render('home/index.html.twig', [
             'tricksPhotos' => $arrayobj
         ]);
+    }
+
+    /**
+     * @Route("/more/", name="home_loadMore")
+     * @param Request $request
+     * @param TrickRepository $trickRepository
+     * @return JsonResponse
+     */
+    public function load(Request $request, TrickRepository $trickRepository): Response
+    {
+        $arrayJson = [];
+        $currentTrick = $request->getSession()->get('currentTrick', 0 );
+        $currentTrick = $currentTrick + 8;
+        $tricks = $trickRepository->findBy([],[], 8, $currentTrick);
+        foreach ($tricks as $trick){
+            $arrayTrick = [
+                'id' => $trick->getId(),
+                'name' => $trick->getName()
+            ];
+            array_push($arrayJson, $arrayTrick);
+        }
+
+        //$request->getSession()->set('currentTrick', $currentTrick);
+
+        return $this->json($arrayJson);
     }
 }
