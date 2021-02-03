5a27e103e32973a89ad88f39d96edaa867fdcf5c
Matthias LEROUX <matthiasleroux@laposte.net>
2021-02-03 22:28:22 +0100

    loading comments with ajax when using the "load more" button

--- src/Repository/CommentRepository.php
+++ src/Repository/CommentRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repository;
 
 use App\Entity\Comment;
+use App\Entity\Trick;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
@@ -17,5 +18,25 @@ class CommentRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Comment::class);
+    }
+
+    /**
+     * @param Trick $trick
+     * @param string|null $order
+     * @param int|null $limit
+     * @param int|null $offset
+     * @return int|mixed|string
+     */
+    public function getCommentsByTrickId(Trick $trick, ?int $limit = null, ?int $offset = null, ?string $order = null){
+        $query = $this->createQueryBuilder('c')
+            ->andWhere('c.trick = :trickId')
+            ->setParameter('trickId', $trick->getId())
+            ;
+        if ($limit){$query->setMaxResults($limit);}
+        if ($offset){$query->setFirstResult($offset);}
+        if ($order){$query->orderBy('c.id',$order);}
+
+        $query = $query->getQuery();
+        return $query->getResult();
     }
 }
