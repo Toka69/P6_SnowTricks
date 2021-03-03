eb908fc7128754404c0072fa28dddbadd710e539
Matthias LEROUX <matthiasleroux@laposte.net>
2021-03-03 08:11:21 +0100

    refactor(render): add video collection and add and delete buttons

--- src/Form/VideoType.php
+++ src/Form/VideoType.php
@@ -4,7 +4,11 @@ namespace App\Form;
 
 use App\Entity\Video;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ButtonType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class VideoType extends AbstractType
@@ -12,8 +16,26 @@ class VideoType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('location')
+            ->add('location', TextType::class, [
+                'label' => '<i class="fas fa-pencil-alt"></i>',
+                'label_html' => true,
+                'required' => true
+            ])
         ;
+
+        $builder->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) {
+            $form = $event->getForm();
+
+            $video = $event->getData();
+
+            if ($video) {
+                $form->add('delete', ButtonType::class, [
+                    'attr' => ['class' => 'delete'],
+                    'label' => '<i class="fas fa-trash-alt"></i>',
+                    'label_html' => true
+                ]);
+            }
+        });
     }
 
     public function configureOptions(OptionsResolver $resolver)
