7824bcde45b8e6820a0b6e41791412228fb6bc1e
Matthias LEROUX <matthiasleroux@laposte.net>
2021-03-13 18:48:50 +0100

    refactor: Modify video link management.

--- src/Entity/Video.php
+++ src/Entity/Video.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\VideoRepository;
 use Doctrine\ORM\Mapping as ORM;
+use DOMDocument;
 
 /**
  * @ORM\Entity(repositoryClass=VideoRepository::class)
@@ -34,11 +35,23 @@ class Video
 
     public function getLocation(): ?string
     {
-        return $this->location;
+        return '<iframe src="'.$this->location.'" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
     }
 
     public function setLocation(string $location): self
     {
+        $dom = new DOMDocument();
+        $dom->loadHTML($location);
+        $searchNode = $dom->getElementsByTagName("iframe");
+        foreach($searchNode as $searchNode){
+            $location = $searchNode->getAttribute("src");
+
+            //for dailymotion
+            if(str_contains($location, "autoplay")){
+                $location = str_replace("?autoplay=1", "", $location);
+            }
+
+        }
         $this->location = $location;
 
         return $this;
