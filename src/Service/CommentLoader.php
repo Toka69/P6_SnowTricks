46f3b5428f2b639a993744742ee83544ba7b9dbd
Matthias LEROUX <matthiasleroux@laposte.net>
2021-04-06 22:57:45 +0200

    refactor(controller): Move Trick functions to Services.

---
+++ src/Service/CommentLoader.php
@@ -0,0 +1,50 @@
+<?php
+
+
+namespace App\Service;
+
+
+use App\Repository\CommentRepository;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+class CommentLoader
+{
+    private $request;
+
+    private $commentRepository;
+
+    public function __construct(RequestStack $requestStack, CommentRepository $commentRepository)
+    {
+        $this->request = $requestStack->getCurrentRequest();
+        $this->commentRepository = $commentRepository;
+    }
+
+    public function arrayJson($trick): array
+    {
+        $arrayJson = [];
+        $currentComment = $this->request->getSession()->get('currentComment', 0);
+        $numberComments = 10;
+        $currentComment = $currentComment + $numberComments;
+        $comments = $this->commentRepository->getCommentsByTrickId($trick, $numberComments, $currentComment, "DESC");
+        foreach ($comments as $comment){
+            $arrayComment = [
+                "content" => $comment->getContent(),
+                "photo" => $comment->getUser()->getPhoto(),
+                "firstName" => $comment->getUser()->getFirstName(),
+                "lastName" => $comment->getUser()->getLastName(),
+                "createdDate" => $comment->getCreatedDate(),
+                "end" => 0
+            ];
+            array_push($arrayJson, $arrayComment);
+        }
+
+        $this->request->getSession()->set('currentComment', $currentComment);
+
+        if ($currentComment + $numberComments >= count($this->commentRepository->getCommentsByTrickId($trick))){
+            array_push($arrayJson, ['end' => 1]);
+            $this->request->getSession()->remove('currentComment');
+        }
+
+        return $arrayJson;
+    }
+}
---
+++ src/Service/TrickService.php
@@ -0,0 +1,99 @@
+<?php
+
+
+namespace App\Service;
+
+
+use App\Entity\Photo;
+use App\Repository\TrickRepository;
+use DateTimeImmutable;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\String\Slugger\SluggerInterface;
+use function Symfony\Component\String\u;
+
+class TrickService
+{
+    private SluggerInterface $slugger;
+
+    private EntityManagerInterface $em;
+
+    private Security $security;
+
+    private TrickRepository $trickRepository;
+
+    private $request;
+
+    public function __construct(SluggerInterface $slugger, EntityManagerInterface $em, Security $security, TrickRepository $trickRepository, RequestStack $requestStack)
+    {
+        $this->slugger = $slugger;
+        $this->em = $em;
+        $this->security = $security;
+        $this->trickRepository = $trickRepository;
+        $this->request = $requestStack->getCurrentRequest();
+    }
+
+    public function persistAddNewTrick($trick)
+    {
+        $trick->setSlug($this->slugger->slug($trick->getName())->lower());
+        $trick->setUser($this->security->getUser());
+        $trick->setCreatedDate(new DateTimeImmutable());
+        $trick->removeEmptyPhotoField();
+        $trick->removeEmptyVideoField();
+
+        $this->em->persist($trick);
+        $this->em->flush();
+    }
+
+    public function updateTrick($trick)
+    {
+        $trick->removeEmptyPhotoField();
+        $trick->removeEmptyVideoField();
+        $trick->setSlug(u($this->slugger->slug($trick->getName()))->lower());
+        $trick->setModifiedDate(new DateTimeImmutable());
+        $this->em->flush();
+    }
+
+    public function addCover($form, $trick)
+    {
+        if ($form->getData()->getFileCover()){
+            $this->em->persist((new Photo)
+                ->setTrick($trick)
+                ->setCover(true)
+                ->setFile($form->getData()->getFileCover()));
+        }
+    }
+
+    public function addNewPhotos($form)
+    {
+        foreach ($form['photos']->getData() as $photo){
+            if($photo->getFile() && ($photo->getId() === null))
+            {
+                $this->em->persist($photo);
+            }
+        }
+    }
+
+    public function trickNameBeforeChanged($trick)
+    {
+        $trickUnchanged = $this->trickRepository->findOneBy(['id' => $trick->getId()]);
+        $this->request->getSession()->set('slugTrickNameBeforeChanged', $trickUnchanged->getSlug());
+    }
+
+    public function removeTrick($trick)
+    {
+        $this->em->remove($trick);
+        $this->em->flush();
+    }
+
+    public function addNewComment($comment, $trick)
+    {
+        $comment->setCreatedDate(new DateTimeImmutable());
+        $comment->setUser($this->security->getUser());
+        $comment->setTrick($trick);
+        $this->em->persist($comment);
+        $this->em->flush();
+    }
+}
