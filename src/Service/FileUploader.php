0818411987b1205e736052cffbea45a1c207bb4b
Matthias LEROUX <matthiasleroux@laposte.net>
2021-04-07 22:51:12 +0200

    Revert FileUploader, because the file name was wrong.

--- src/Service/FileUploader.php
+++ src/Service/FileUploader.php
@@ -24,7 +24,7 @@ class FileUploader
 
     public function upload(UploadedFile $file)
     {
-        $originalFilename = $file->getPathInfo();
+        $originalFilename = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
         $safeFilename = $this->slugger->slug($originalFilename);
         $newFilename = $safeFilename . '-' . uniqid() . '.' . $file->guessExtension();
         $file->move($this->targetDirectory, $newFilename);
