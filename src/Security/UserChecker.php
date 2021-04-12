e73c8660631c815ed3d9fba0ef6a57b3f0311d80
Matthias LEROUX <matthiasleroux@laposte.net>
2021-04-12 11:31:37 +0200

    check if the account is valid to login.

---
+++ src/Security/UserChecker.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace App\Security;
+
+use App\Entity\User as AppUser;
+use Symfony\Component\Security\Core\Exception\AccountExpiredException;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAccountStatusException;
+use Symfony\Component\Security\Core\User\UserCheckerInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class UserChecker implements UserCheckerInterface
+{
+    public function checkPreAuth(UserInterface $user)
+    {
+        if(!$user->isVerified()){
+            throw new CustomUserMessageAccountStatusException("Your account is not yet been verified!");
+        }
+    }
+
+    public function checkPostAuth(UserInterface $user)
+    {
+
+    }
+}
